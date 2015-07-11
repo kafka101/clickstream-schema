49c888b698622c4c7cddffa22874e4fb1edf11d0
Lars Pfannenschmidt <lars@mobilecologne.de>
2015-07-11 16:54:30 +0200

    reformatting

--- domain/src/main/java/io/kafka101/clickstream/schema/domain/Click.java
+++ domain/src/main/java/io/kafka101/clickstream/schema/domain/Click.java
@@ -5,15 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Click {
 
-    @JsonProperty(required = true)
     public final String time;
-    @JsonProperty(required = true)
     public final String ip;
-    @JsonProperty(required = true)
     public final String page;
 
     @JsonCreator
-    public Click(@JsonProperty("time") String time, @JsonProperty("ip") String ip, @JsonProperty("page") String page) {
+    public Click(@JsonProperty("time") String time,
+            @JsonProperty("ip") String ip,
+            @JsonProperty("page") String page) {
         this.time = time;
         this.ip = ip;
         this.page = page;
