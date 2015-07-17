86bc66e857445f5a65744151121c2b9ae010327d
Lars Pfannenschmidt <lars@mobilecologne.de>
2015-07-17 13:08:29 +0200

    use merged PR again for SR

--- confluent-deps.sh
+++ confluent-deps.sh
@@ -5,8 +5,7 @@ mkdir confluent
 cd confluent
 git clone https://github.com/confluentinc/common.git
 git clone https://github.com/confluentinc/rest-utils.git
-#git clone https://github.com/confluentinc/schema-registry.git
-git clone https://github.com/kafka101/schema-registry.git
+git clone https://github.com/confluentinc/schema-registry.git
 cd common
 mvn clean install
 cd ../rest-utils
