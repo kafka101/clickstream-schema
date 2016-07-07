d3f0968e702954d998f55c48b270fd8d67357661
Frank Wisniewski <frank.wisniewski@gmail.com>
2016-07-06 23:17:42 -0700

    Upgraded to Confluent version 2.0.1
     * Upgraded Confluent version to 2.0.1
     * Upgraded Kafka to 0.9.0.1
     * Got rid of shell script to build Confluent's resources (test resources are now available in their repo)
     * Adapted tests to work with new version

--- it-test/src/test/java/io/kafka101/clickstream/test/EmbeddedKafkaTest.java
+++ it-test/src/test/java/io/kafka101/clickstream/test/EmbeddedKafkaTest.java
@@ -2,11 +2,13 @@ package io.kafka101.clickstream.test;
 
 import io.confluent.kafka.schemaregistry.RestApp;
 import kafka.admin.AdminUtils;
+import kafka.api.FixedPortTestUtils;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServer;
 import kafka.utils.*;
 import kafka.zk.EmbeddedZookeeper;
 import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
@@ -23,22 +25,22 @@ public abstract class EmbeddedKafkaTest {
     private static EmbeddedZookeeper zkServer;
     private static ZkClient zkClient;
     private static KafkaServer kafkaServer;
+    private static ZkUtils zkUtils;
+    private static ZkConnection zkConnection;
     private static RestApp schemaRegistry;
 
     private static List<KafkaServer> servers = new ArrayList();
 
     @BeforeClass
     public static void setUp() throws Exception {
-        // setup Zookeeper
-        zkConnect = TestZKUtils.zookeeperConnect();
-        zkServer = new EmbeddedZookeeper(zkConnect);
-        zkClient = new ZkClient(zkServer.connectString(), 30000, 30000, ZKStringSerializer$.MODULE$);
+        zkServer = new EmbeddedZookeeper();
+        zkConnect = "127.0.0.1:" + zkServer.port();
+        zkConnection = new ZkConnection(zkConnect);
+        zkClient = new ZkClient(zkConnect, 30000, 30000, ZKStringSerializer$.MODULE$);
+        zkUtils = new ZkUtils(zkClient, zkConnection, false);
 
         // setup Broker
-        int port = TestUtils.choosePort();
-        int brokerId = 0;
-        Properties props = TestUtils.createBrokerConfig(brokerId, port, true);
-
+        Properties props = FixedPortTestUtils.createBrokerConfigs(1, zkConnect, true, false).apply(0);
         KafkaConfig config = new KafkaConfig(props);
         Time mock = new MockTime();
         kafkaServer = TestUtils.createServer(config, mock);
@@ -46,7 +48,7 @@ public abstract class EmbeddedKafkaTest {
         kafkaConnect = kafkaServer.config().hostName() + ":" + kafkaServer.config().port();
 
         // setup Schema-Registry
-        schemaRegistry = new RestApp(8081, zkServer.connectString(), "_schemas");
+        schemaRegistry = new RestApp(8081, zkConnect, "_schemas");
         schemaRegistry.start();
         restConnect = schemaRegistry.restConnect;
     }
@@ -63,7 +65,7 @@ public abstract class EmbeddedKafkaTest {
     protected void createTopic(String topic) {
         Properties properties = new Properties();
         properties.put("cleanup.policy", "compact");
-        AdminUtils.createTopic(zkClient, topic, 1, 1, properties);
+        AdminUtils.createTopic(zkUtils, topic, 1, 1, properties);
         TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(servers), topic, 0,
                 10000);
     }
